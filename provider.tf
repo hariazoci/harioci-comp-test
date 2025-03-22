ced3fda2e572c21e5597e2c09906814fb8b0ccbe
hariazoci <167167973+hariazoci@users.noreply.github.com>
2025-03-22 16:34:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "oci" {
   # Configure your OCI provider settings here
   # Example configuration:
-  # region         = "us-ashburn-1"
-  # tenancy_ocid   = "ocid1.tenancy.oc1..<your_tenancy_ocid>"
-  # user_ocid      = "ocid1.user.oc1..<your_user_ocid>"
-  # fingerprint    = "<your_api_key_fingerprint>"
-  # private_key_path = "~/.oci/oci_api_key.pem"
+  region         = "us-ashburn-1"
+  tenancy_ocid   = "ocid1.tenancy.oc1..aaaaaaaau6tqqxwoo36xtiqtqgdfnaepwwdcgwcldg6stlrbxju4jzs76k2q"
+  user_ocid      = "ocid1.user.oc1..aaaaaaaaliqqtocdaz3tafj7to5byxlf6y5qtommgassrxlv4iq3pxvhqtqa"
+  fingerprint    = "c6:43:46:10:1b:15:d8:23:2f:9a:75:00:9c:d8:3f:10"
+  private_key_path = "~/.oci/oci_api_key.pem"
   
   # Uncomment and update the above lines with your actual OCI credentials
 }
