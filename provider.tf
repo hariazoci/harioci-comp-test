ddd83c72d539c49df5176006371036bc9efac83a
hariazoci <167167973+hariazoci@users.noreply.github.com>
2025-03-22 16:10:19 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "oci" {
+  # Configure your OCI provider settings here
+  # Example configuration:
+  # region         = "us-ashburn-1"
+  # tenancy_ocid   = "ocid1.tenancy.oc1..<your_tenancy_ocid>"
+  # user_ocid      = "ocid1.user.oc1..<your_user_ocid>"
+  # fingerprint    = "<your_api_key_fingerprint>"
+  # private_key_path = "~/.oci/oci_api_key.pem"
+  
+  # Uncomment and update the above lines with your actual OCI credentials
+}
