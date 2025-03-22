af942f018bf05c5cc170ea78ef502a912c80b5e5
hariazoci <167167973+hariazoci@users.noreply.github.com>
2025-03-22 16:36:02 +0530

    Update compartment.tf

--- compartment.tf
+++ compartment.tf
@@ -1,5 +1,5 @@
 resource "oci_identity_compartment" "example_compartment" {
-  compartment_id = "<parent_compartment_ocid>" # Replace with your parent compartment OCID (typically your tenancy OCID)
+  compartment_id = "ocid1.tenancy.oc1..aaaaaaaau6tqqxwoo36xtiqtqgdfnaepwwdcgwcldg6stlrbxju4jzs76k2q" # Replace with your parent compartment OCID (typically your tenancy OCID)
   description    = "jnks compartment"
   name           = "jkns-comp"
 }
