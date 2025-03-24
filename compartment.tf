c133c13f95d25da4a8d903f39eb814e782d69f9a
hariazoci <hariazoci@outlook.com>
2025-03-24 17:06:59 +0530

    action

--- compartment.tf
+++ compartment.tf
@@ -1,14 +1,7 @@
 
 resource "oci_identity_compartment" "example_compartment" {
   compartment_id = var.tenancy_ocid # Replace with your parent compartment OCID (typically your tenancy OCID)
-  description    = "jnks compartment"
-  name           = "jkns-comp"
-  enable_delete  = true
-}
-
-resource "oci_identity_compartment" "demo-compartment" {
-  compartment_id = var.tenancy_ocid # Replace with your parent compartment OCID (typically your tenancy OCID)
-  description    = "hello compartment"
-  name           = "hellojkns-comp"
+  description    = "action-comp"
+  name           = "action-cmp"
   enable_delete  = true
 }
