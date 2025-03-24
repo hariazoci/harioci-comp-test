50e81e7378c44c227a5eb1f6ae33d539232461e3
hariazoci <167167973+hariazoci@users.noreply.github.com>
2025-03-24 14:54:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
+variable "access_key" {
+  description = "S3 interoperability access key for OCI Object Storage"
+  type        = string
+}
