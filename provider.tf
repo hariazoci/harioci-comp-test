df9153dc0e7fc14cffb41095897eaaa5a638db70
hariazoci <167167973+hariazoci@users.noreply.github.com>
2025-03-24 14:43:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,21 +5,20 @@ terraform {
       version = ">= 4.0.0"
     }
   }
-}
 
-terraform {
   backend "s3" {
-endpoint = "https://orasenatdoracledigital04.compat.objectstorage.region.oraclecloud.com"
-    bucket          = "haribucket"
-    region          = "us-ashburn-1"
-    key             = "terraform/state/terraform.tfstate" # Path to the state file
- skip_region_validation     = true
+    endpoint = "https://orasenatdoracledigital04.compat.objectstorage.us-ashburn-1.oraclecloud.com"
+    bucket   = "haribucket"
+    region   = "us-ashburn-1"
+    key      = "terraform/state/terraform.tfstate"
+    
+    skip_region_validation     = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
     skip_metadata_api_check    = true
-    use_path_style             = true
+    force_path_style           = true
   }
 }
+
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
